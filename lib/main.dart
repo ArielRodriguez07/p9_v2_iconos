bd933ac43b64e85e1d3a795a9c0b3531cbac8c2c
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-02-26 20:33:50 +0000

    imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,185 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple, // Color morado para el AppBar
+          elevation: 0, // Elimina la sombra del AppBar
+          title: Text(
+            "Ariel Rodriguez", // Título en el AppBar
+            style: TextStyle(
+              fontSize: 25, // Tamaño de la letra
+              color: Colors.white, // Color de la letra
+              fontWeight: FontWeight.bold, // Negrita
+            ),
+          ),
+          centerTitle: true, // Centrar el título
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: 20), // Espacio entre el AppBar y el subtítulo
+            // Subtítulo en negro fuerte
+            Padding(
+              padding: EdgeInsets.all(10), // Espaciado
+              child: Text(
+                "Ariel Rodriguez Ceniceros 22308051280706",
+                style: TextStyle(
+                  fontSize: 18, // Tamaño de la letra
+                  color: Colors.black, // Color de la letra
+                  fontWeight: FontWeight.bold, // Negrita
+                ),
+                textAlign: TextAlign.center, // Centrar el texto
+              ),
+            ),
+            SizedBox(height: 20), // Espacio entre el subtítulo y los iconos
+            // Primera fila de iconos con subtítulos
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.person, // Icono de usuario
+                      color: Colors.pink,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Usuario",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.audiotrack, // Icono de música
+                      color: Colors.green,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Música",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.beach_access, // Icono de playa
+                      color: Colors.blue,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Playa",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 40), // Espacio entre las filas
+            // Segunda fila de iconos con subtítulos
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.home, // Icono de hogar
+                      color: Colors.orange,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Hogar",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.work, // Icono de trabajo
+                      color: Colors.purple,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Trabajo",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.school, // Icono de escuela
+                      color: Colors.teal,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Escuela",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 40), // Espacio entre las filas
+            // Tercera fila de iconos con subtítulos
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.favorite, // Icono de favorito
+                      color: Colors.red,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Favorito",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.star, // Icono de estrella
+                      color: Colors.yellow,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Estrella",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.location_on, // Icono de ubicación
+                      color: Colors.brown,
+                      size: 40.0,
+                    ),
+                    Text(
+                      "Ubicación",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } // fin de widgets
+} // fin de la clase MisIconos
